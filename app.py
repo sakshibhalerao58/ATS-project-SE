4cb7ca19ec6f6e4520a1a6c61235d2a86660b402
Sakshi Shivaji Bhalerao. <sakshibhalerao58@gmail.com>
2025-04-11 11:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,54 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-import fitz  # PyMuPDF
-import base64
 import streamlit as st
-import os
-import io
-from PIL import Image 
-
 import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+
+load_dotenv() ## load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input,pdf_cotent,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,pdf_content[0],prompt])
+def get_gemini_repsonse(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
     return response.text
 
-def input_pdf_setup(uploaded_file):
-    if uploaded_file is not None:
-        file_bytes = uploaded_file.read()
-        pdf = fitz.open(stream=file_bytes, filetype="pdf")
-        first_page = pdf[0]
-        
-        # Render first page as image
-        pix = first_page.get_pixmap(dpi=150)
-        img_byte_arr = io.BytesIO(pix.tobytes("jpeg"))
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
 
-        # Base64 encode the image
-        img_data = base64.b64encode(img_byte_arr.getvalue()).decode()
+#Prompt Template
 
-        pdf_parts = [
-            {
-                "mime_type": "image/jpeg",
-                "data": img_data
-            }
-        ]
-        return pdf_parts
-    else:
-        raise FileNotFoundError("No file uploaded")
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
+resume:{text}
+description:{jd}
 
-
-## Streamlit App
-
-st.set_page_config(page_title="ATS Resume EXpert")
-st.header("ATS Tracking System")
-input_text=st.text_area("Job Description: ",key="input")
-uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
-
-
-if uploaded_file is not None:
-    st.write("PDF Uploaded Successfully")
-
-
-submit1 = st.button("Tell Me About the Resume")
-
-#submit2 = st.button("How Can I Improvise my Skills")
-
-submit3 = st.button("Percentage match")
-
-input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
 
-input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
-"""
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
 
-if submit1:
+submit = st.button("Submit")
+
+if submit:
     if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
-    else:
-        st.write("Please uplaod the resume")
-
-elif submit3:
-    if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
-    else:
-        st.write("Please uplaod the resume")
-
-
-
-   
-
-
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_repsonse(input_prompt)
+        st.subheader(response)
